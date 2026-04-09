3f462d3e6330e00f4dc568bb9f27e6be39d5e7e4
Eduardo Jesus <126268026+EdwJezus@users.noreply.github.com>
2026-04-09 17:30:36 -0300

    Criado Função cria_triangulo para simplificar a criação dos objetos

--- projeto.py
+++ projeto.py
@@ -2,6 +2,8 @@ import glfw
 from OpenGL.GL import *
 
 selecionado = 0
+obj = 0
+color = 0,0,0
 estado_clique = 0
 p1x = 0
 p1y = 0
@@ -44,14 +46,9 @@ def init():
 
 #################################################
 
-def render():
-    global selecionado
-    glClear(GL_COLOR_BUFFER_BIT) # limpa o buffer
-    glLoadIdentity() # limpa a identidade
-
-    # triangulo 1
+def cria_triangulo(tx, ty, rt, p1x, p1y, p2x, p2y, p3x, p3y, obj, color):
     glPushMatrix()
-    glColor3f(1, 0, 0) # cor do objeto
+    glColor3f(*color) # cor do objeto
     glTranslatef(0 + tx, 0 + ty, 1)
     glRotatef(0 + rt, 0, 0, 1)
     glBegin(GL_TRIANGLES)
@@ -60,7 +57,7 @@ def render():
     glVertex2f(p3x, p3y) # vertice 3
     glEnd()
     # borda
-    if selecionado == 1:
+    if selecionado == obj:
         glColor3f(0, 0, 0)
         glLineWidth(4)
         glBegin(GL_LINE_LOOP)
@@ -68,32 +65,17 @@ def render():
         glVertex2f(p2x, p2y) # vertice 2
         glVertex2f(p3x, p3y) # vertice 3
         glEnd()
-        glPopMatrix()
-    else:
-        glPopMatrix()
+    glPopMatrix()
 
-    # triangulo 2
-    glPushMatrix()
-    glColor3f(0, 1, 0) # cor do objeto
-    glTranslatef(0 + tx2, 0 + ty2, 1)
-    glRotatef(0 + rt2, 0, 0, 1)
-    glBegin(GL_TRIANGLES)
-    glVertex2f(pp1x, pp1y) # vertice 1
-    glVertex2f(pp2x, pp2y) # vertice 2
-    glVertex2f(pp3x, pp3y) # vertice 3
-    glEnd()
-    # borda
-    if selecionado == 2:
-        glColor3f(0, 0, 0)
-        glLineWidth(4)
-        glBegin(GL_LINE_LOOP)
-        glVertex2f(pp1x, pp1y) # vertice 1
-        glVertex2f(pp2x, pp2y) # vertice 2
-        glVertex2f(pp3x, pp3y) # vertice 3
-        glEnd()
-        glPopMatrix()
-    else:
-        glPopMatrix()
+#################################################
+
+def render():
+    global selecionado
+    glClear(GL_COLOR_BUFFER_BIT) # limpa o buffer
+    glLoadIdentity() # limpa a identidade
+
+    cria_triangulo(tx, ty, rt, p1x, p1y, p2x, p2y, p3x, p3y, 1, (1,0,0))
+    cria_triangulo(tx2, ty2, rt2, pp1x, pp1y, pp2x, pp2y, pp3x, pp3y, 2, (0,1,0))
 
 #################################################
 
@@ -135,6 +117,7 @@ def teclado(window):
 
 #################################################
 
+'''
 def mouse(window):
     global estado_clique
     if glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
@@ -144,6 +127,7 @@ def mouse(window):
             estado_clique = 0      
     if estado_clique == 1:
         teclado(window)
+'''
 
 #################################################
 
@@ -154,7 +138,8 @@ def main():
     init()
     while not glfw.window_should_close(window):
         glfw.poll_events()
-        mouse(window)
+        #mouse(window)
+        teclado(window)
         render()
         glfw.swap_buffers(window) # troca frames
     glfw.terminate() # finaliza api
